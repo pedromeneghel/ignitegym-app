b3571a87290f229995dfdba26a34bfb6132c5c4d
Pedro Meneghel <pedromeneghel@gmail.com>
2023-09-11 11:29:10 -0300

    feat: adiciona componente HomeHeader

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,30 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { HStack, Heading, Icon, Text, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
+
+import { UserPhoto } from "./UserPhoto";
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: "https://github.com/pedromeneghel.png" }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Pedro
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
