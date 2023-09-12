a9e6f7d79558994827479b54394f9d58d33b8bdb
Pedro Meneghel <pedromeneghel@gmail.com>
2023-09-12 07:24:20 -0300

    feat: adiciona conteúdo da tela History

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,43 @@
-import { Center, Text } from "native-base";
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Heading, SectionList, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "26.08.2023",
+      data: ["Puxada frontal", "Remada unilateral"],
+    },
+    {
+      title: "25.08.2023",
+      data: ["Sulpino reto", "Remada unilateral"],
+    },
+  ]);
+
   return (
-    <Center>
-      <Text>Exercícios</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: "center" }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda.{"\n"}Vamos fazer exercícios
+            hoje?
+          </Text>
+        )}
+        px={8}
+      />
+    </VStack>
   );
 }
