b0aed9b8b260d8ea1957bc0b3233b1312b369fec
Pedro Meneghel <pedromeneghel@gmail.com>
2023-09-13 07:24:56 -0300

    feat: adiciona conteúdo na tela Profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,81 @@
-import { Center, Text } from "native-base";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Skeleton,
+  Text,
+  VStack,
+} from "native-base";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
   return (
-    <Center>
-      <Text>Perfil</Text>
-    </Center>
+    <VStack>
+      <ScreenHeader title="Perfil" />
+      <ScrollView contentContainerStyle={{ paddingBottom: 100 }}>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://github.com/pedromeneghel.png" }}
+              alt="Imagem do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input placeholder="Nome" bg="gray.600" />
+
+          <Input bg="gray.600" placeholder="E-mail" isDisabled />
+        </Center>
+
+        <Center px={10} mb={9}>
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            mt={12}
+            alignSelf="flex-start"
+          >
+            Alterar senha
+          </Heading>
+          <Input placeholder="Senha antiga" bg="gray.600" secureTextEntry />
+          <Input placeholder="Nova senha" bg="gray.600" secureTextEntry />
+          <Input
+            placeholder="Confirme nova senha"
+            bg="gray.600"
+            secureTextEntry
+          />
+          <Button title="Atualizar" mt={4} />
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
