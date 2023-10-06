d2f385f7fae9bd88622a60b6823c97b258e98988
Pedro Meneghel <pedromeneghel@gmail.com>
2023-10-06 09:11:31 -0300

    feat: ajusta rotas para considerar contexto de autenticação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,5 @@
+import { Loading } from "@components/Loading";
+import { useAuth } from "@hooks/useAuth";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { Box, useTheme } from "native-base";
 
@@ -6,13 +8,19 @@ import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
   const { colors } = useTheme();
+  const { user, isLoadingUserStorage } = useAuth();
+
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
+
+  if (isLoadingUserStorage) {
+    return <Loading />;
+  }
+
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
-        {/* <AppRoutes /> */}
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
